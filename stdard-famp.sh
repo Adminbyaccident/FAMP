788cc21c684ba14974bc8537827d1ac6bdf38df9
Adminbyaccident <46286613+Adminbyaccident@users.noreply.github.com>
2019-09-23 00:03:25 +0200

    Create stdard-famp.sh

---
+++ stdard-famp.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+
+# Update packages list
+pkg update
+
+# Update actually installed packages
+pkg upgrade -y
+
+# Install Apache
+pkg install -y apache24
+
+# Add service to be fired up at boot time
+sysrc apache24_enable="YES"
+
+# Install MySQL
+pkg install -y mysql80-server
+
+# Add service to be fired up at boot time
+sysrc mysql_enable="YES"
+
+# Install PHP 7.3 and its 'funny' dependencies
+pkg install -y php73 php73-mysqli mod_php73 php73-extensions
+
+# Install the 'old fashioned' Expect to automate the mysql_secure_installation part
+pkg install -y expect
+
+# Create configuration file for Apache HTTP to 'speak' PHP
+tocuh /usr/local/etc/apache24/modules.d/001_mod-php.conf
+
+# Add the configuration into the file
+echo '<IfModule dir_module>' >> /usr/local/etc/apache24/modules.d/001_mod-php.conf
+echo '   DirectoryIndex index.php index.html' >> /usr/local/etc/apache24/modules.d/001_mod-php.conf
+echo '   <FilesMatch "\.php$">' >> /usr/local/etc/apache24/modules.d/001_mod-php.conf
+echo '        SetHandler application/x-httpd-php' >> /usr/local/etc/apache24/modules.d/001_mod-php.conf
+echo '    </FilesMatch>' >> /usr/local/etc/apache24/modules.d/001_mod-php.conf
+echo '    <FilesMatch "\.phps$">' >> /usr/local/etc/apache24/modules.d/001_mod-php.conf
+echo '        SetHandler application/x-httpd-php-source' >> /usr/local/etc/apache24/modules.d/001_mod-php.conf
+echo '    </FilesMatch>' >> /usr/local/etc/apache24/modules.d/001_mod-php.conf
+echo '</IfModule>' >> /usr/local/etc/apache24/modules.d/001_mod-php.conf
+
+# Set the PHP's default configuration
+cp /usr/local/etc/php.ini-production /usr/local/etc/php.ini
+
+# Fire up the services
+service apache24 start
+service mysql-server start
+
+# Make the hideous 'safe' install for MySQL
+#!/bin/sh
+
+SECURE_MYSQL=$(expect -c "
+set timeout 10
+spawn mysql_secure_installation
+expect \"Press y|Y for Yes, any other key for No:\"
+send \"y\r\"
+expect \"Please enter 0 = LOW, 1 = MEDIUM and 2 = STRONG:\"
+send \"0\r\"
+expect \"New password:\"
+send \"albertXP-24\r\"
+expect \"Re-enter new password:\"
+send \"albertXP-24\r\"
+expect \"Do you wish to continue with the password provided?(Press y|Y for Yes, any other key for No) :\"
+send \"Y\r\"
+expect \"Remove anonymous users?\"
+send \"y\r\"
+expect \"Disallow root login remotely?\"
+send \"y\r\"
+expect \"Remove test database and access to it?\"
+send \"y\r\"
+expect \"Reload privilege tables now?\"
+send \"y\r\"
+expect eof
+")
+
+echo "$SECURE_MYSQL"
