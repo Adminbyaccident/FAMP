b1b23ad35a5a8228005567d2734a4b0e6ea3934b
Adminbyaccident <46286613+Adminbyaccident@users.noreply.github.com>
2019-10-13 23:46:22 +0200

    Create event-php-fpm.sh

---
+++ event-php-fpm.sh
@@ -0,0 +1,95 @@
+#!/bin/sh
+
+# Change the default pkg repository from quarterly to latest
+sed -ip 's/quarterly/latest/g' /etc/pkg/FreeBSD.conf
+
+# Update packages (it will first download the pkg repo from latest)
+# secondly it will upgrade any installed packages.
+pkg upgrade -y
+
+# Install Apache
+pkg install -y apache24
+
+# Add service to be fired up at boot time
+sysrc apache24_enable="YES"
+
+# Install MySQL
+pkg install -y mysql80-server
+
+# Add service to be fired up at boot time
+sysrc mysql_enable="YES"
+
+# Install PHP 7.3 and its 'funny' dependencies
+pkg install -y php73 php73-mysqli php73-extensions
+
+# Install the 'old fashioned' Expect to automate the mysql_secure_installation part
+pkg install -y expect
+
+# Set a ServerName directive in Apache HTTP. Place a name to your server.
+sed -i -e 's/#ServerName www.example.com:80/ServerName California/g' /usr/local/etc/apache24/httpd.conf
+
+# Configure Apache HTTP to use MPM Event instead of the Prefork default
+# 1.- Disable the Prefork MPM
+sed -i -e '/prefork/s/LoadModule/#LoadModule/' /usr/local/etc/apache24/httpd.conf
+
+# 2.- Enable the Event MPM
+sed -i -e '/event/s/#LoadModule/LoadModule/' /usr/local/etc/apache24/httpd.conf
+
+# 3.- Enable the proxy module for PHP-FPM to use it
+sed -i -e '/mod_proxy.so/s/#LoadModule/LoadModule/' /usr/local/etc/apache24/httpd.conf
+
+# 4.- Enable the FastCGI module for PHP-FPM to use it
+sed -i -e '/mod_proxy_fcgi.so/s/#LoadModule/LoadModule/' /usr/local/etc/apache24/httpd.conf
+
+# Enable PHP to use the FPM process manager
+sysrc php_fpm_enable="YES"
+
+# Create configuration file for Apache HTTP to 'speak' PHP
+tocuh /usr/local/etc/apache24/modules.d/003_php-fpm.conf
+
+# Add the configuration into the file
+echo '<IfModule proxy_fcgi_module>' >> /usr/local/etc/apache24/modules.d/003_php-fpm.conf
+echo '   <IfModule dir_module>' >> /usr/local/etc/apache24/modules.d/003_php-fpm.conf
+echo '   	DirectoryIndex index.php' >> /usr/local/etc/apache24/modules.d/003_php-fpm.conf
+echo '   </IfModule>' >> /usr/local/etc/apache24/modules.d/003_php-fpm.conf
+echo '   <FilesMatch "\.(php|phtml|inc)$">' >> /usr/local/etc/apache24/modules.d/003_php-fpm.conf
+echo '    	SetHandler "proxy:fcgi://127.0.0.1:9000"' >> /usr/local/etc/apache24/modules.d/003_php-fpm.conf
+echo '   </FilesMatch>' >> /usr/local/etc/apache24/modules.d/003_php-fpm.conf
+echo '</IfModule>' >> /usr/local/etc/apache24/modules.d/003_php-fpm.conf
+
+# Set the PHP's default configuration
+cp /usr/local/etc/php.ini-production /usr/local/etc/php.ini
+
+# Fire up the services
+service apache24 start
+service mysql-server start
+service php-fpm start
+
+# Make the hideous 'safe' install for MySQL
+#!/bin/sh
+
+SECURE_MYSQL=$(expect -c "
+set timeout 10
+spawn mysql_secure_installation
+expect \"Press y|Y for Yes, any other key for No:\"
+send \"y\r\"
+expect \"Please enter 0 = LOW, 1 = MEDIUM and 2 = STRONG:\"
+send \"0\r\"
+expect \"New password:\"
+send \"albertXP-24\r\"
+expect \"Re-enter new password:\"
+send \"albertXP-24\r\"
+expect \"Do you wish to continue with the password provided?(Press y|Y for Yes, any other key for No) :\"
+send \"Y\r\"
+expect \"Remove anonymous users?\"
+send \"y\r\"
+expect \"Disallow root login remotely?\"
+send \"y\r\"
+expect \"Remove test database and access to it?\"
+send \"y\r\"
+expect \"Reload privilege tables now?\"
+send \"y\r\"
+expect eof
+")
+
+echo "$SECURE_MYSQL"
