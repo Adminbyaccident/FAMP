7c31cfedfea4f957a2e7b2d5c23755f41ec133de
Adminbyaccident <46286613+Adminbyaccident@users.noreply.github.com>
2019-10-28 23:02:25 +0100

    Create new-mysqldb.sh

---
+++ new-mysqldb.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Create a database inside MySQL version 8 using Expect
+
+# Install Expect
+pkg install -y expect
+
+# As if we were before the keyboard
+
+NEW_DATABASE=$(expect -c "
+set timeout 10
+spawn mysql -u root -p
+expect \"Enter password:\"
+send \"albertXP-24\r\"
+expect \"root@localhost \[(none)\]>\"
+send \"CREATE DATABASE ballanga;\r\"
+expect \"root@localhost \[(none)\]>\"
+send \"CREATE USER 'xava'@'localhost' IDENTIFIED WITH mysql_native_password BY 'cavallXP-35';\r\"
+expect \"root@localhost \[(none)\]>\"
+send \"GRANT ALL PRIVILEGES ON ballanga.* TO 'xava'@'localhost';\r\"
+expect \"root@localhost \[(none)\]>\"
+send \"FLUSH PRIVILEGES;\r\"
+expect \"root@localhost \[(none)\]>\"
+send \"exit\r\"
+expect eof
+")
+
+echo "$NEW_DATABASE"
+
+# Remove Expect if it's no longer needed
+# pkg remove -y expect tcl86
