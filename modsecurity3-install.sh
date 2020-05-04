d2fba4cd3eb8feef3a71d592243455779fd77b20
Adminbyaccident <46286613+Adminbyaccident@users.noreply.github.com>
2020-05-05 00:31:17 +0200

    Update modsecurity3-install.sh

--- modsecurity3-install.sh
+++ modsecurity3-install.sh
@@ -24,10 +24,12 @@ sed -ip 's/SecRuleEngine DetectionOnly/SecRuleEngine On/g' /usr/local/etc/modsec
 
 # Configure ModSecurity3's module
 touch /usr/local/etc/apache24/modules.d/280_mod_security.conf
-echo '<IfModule security3_module>' >> /usr/local/etc/apache24/modules.d/280_mod_security.conf
-echo '	modsecurity on' >> /usr/local/etc/apache24/modules.d/280_mod_security.conf
-echo '	modsecurity_rules_file /usr/local/etc/modsecurity/crs-setup.conf' >> /usr/local/etc/apache24/modules.d/280_mod_security.conf
-echo '</IfModule>' >> /usr/local/etc/apache24/modules.d/280_mod_security.conf 
+
+echo " 
+<IfModule security3_module>
+    modsecurity on
+    modsecurity_rules_file /usr/local/etc/modsecurity/crs-setup.conf
+</IfModule>" >> /usr/local/etc/apache24/modules.d/280_mod_security.conf 
 
 # Restart Apache HTTP
 apachectl restart
