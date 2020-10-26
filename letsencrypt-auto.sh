8dfbabb9a838d1a1775d40365fd39ecf2a020eec
Adminbyaccident <46286613+Adminbyaccident@users.noreply.github.com>
2020-10-26 12:39:46 +0100

    Create letsencrypt-auto.sh

---
+++ letsencrypt-auto.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+
+# Instructions on how to use this script 
+
+# chmod +x SCRIPTNAME.sh
+
+# sudo ./SCRIPTNAME.sh
+
+## This script will only work on a FreeBSD + Apache HTTP install.
+## Adjust your-domain.com to your real one.
+
+# Change the default pkg repository from quarterly to latest
+sed -ip 's/quarterly/latest/g' /etc/pkg/FreeBSD.conf
+
+# Update packages (it will first download the pkg repo from latest)
+# secondly it will upgrade any installed packages.
+pkg upgrade -y
+
+# Install the Certbot utility for Apache HTTP
+pkg install -y py37-certbot py37-certbot-apache
+
+# Enable SSL/TLS in Apache HTTP
+sed -i -e '/mod_ssl.so/s/#LoadModule/LoadModule/' /usr/local/etc/apache24/httpd.conf
+
+# Enable Virtual Hosts
+sed -i -e '/httpd-vhosts.conf/s/#Include/Include/' /usr/local/etc/apache24/httpd.conf
+ 
+# Make a backup of the original httpd-vhosts.conf file
+cp /usr/local/etc/apache24/extra/httpd-vhosts.conf /usr/local/etc/apache24/extra/httpd-vhosts.conf.backup
+
+# Remove the original httpd-vhosts file
+rm /usr/local/etc/apache24/extra/httpd-vhosts.conf
+
+# Create an empty httpd-vhosts file
+touch /usr/local/etc/apache24/extra/httpd-vhosts.conf
+
+# Add our configuration in the httpd-vhosts file
+echo "
+<VirtualHost *:80>
+    ServerAdmin your_email@your_domain.com
+    DocumentRoot "/usr/local/www/apache24/data/your_domain.com"
+    ServerName your_domain.com
+    ServerAlias www.your_domain.com
+    ErrorLog "/var/log/your_domain.com-error_log"
+    CustomLog "/var/log/your_domain.com-access_log" common
+</VirtualHost>" >> /usr/local/etc/apache24/extra/httpd-vhosts.conf
+
+# Create the DocumentRoot for your site
+mkdir /usr/local/www/apache24/data/your_domain.com
+
+# Place an index.html file to test your site
+touch /usr/local/www/apache24/data/your-domain.com/index.html
+
+# Put the minimal content in index.html
+echo "
+<html><body><h1>It works!</h1></body></html>" >> /usr/local/www/apache24/data/your-domain.com/index.html
+
+# Certbot will configure Apache HTTP's virtual hosts configuration file with a redirect
+# or not depending on your needs. In this script a redirect from HTTP to HTTPS is chosen.
+# If you don't want a redirect as such, replace the "2" for a "1" in the send part of the expect script.
+
+# Install the 'old fashioned' Expect to automate the certbot part
+pkg install -y expect
+
+# Launch the certbot utility for Apache HTTP
+LETENCRYPT_APACHE=$(expect -c "
+set timeout 10
+spawn certbot --apache -d your-domain -d www.your-domain
+expect \"Select\"
+send \2\r\"
+expect eof
+")
+
+echo "
+Visit your site with your browser http://your-domain.com and you should see the redirect to HTTPS and the index.html page rendering It works!
+"
+
+## References:
+## https://www.digitalocean.com/community/tutorials/how-to-secure-apache-with-let-s-encrypt-on-freebsd-12-0
