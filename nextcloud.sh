542d9ae9724abd1b69d842267c5b0b2491585735
Adminbyaccident <46286613+Adminbyaccident@users.noreply.github.com>
2020-10-26 19:13:09 +0100

    Update nextcloud.sh
    Nextcloud version changed from 19.0.3 to 20.0.1. 
    Warning explanation improved.

--- nextcloud.sh
+++ nextcloud.sh
@@ -13,6 +13,7 @@
 # The base ones will be met by installing a FAMP stack server.
 # You may use one of the following three scripts for this: 
 #
+#
 # Apache Pre-fork MPM 
 # https://github.com/Adminbyaccident/FAMP/blob/master/stdard-famp.sh
 #
@@ -22,13 +23,16 @@
 # Apache Event MPM + PHP-FPM on UNIX socket
 # https://github.com/Adminbyaccident/FAMP/blob/master/event-php-fpm-unix-socket.sh
 #
-# To harden this set up use the following security script for the FAMP stack:
+#
+# Once the base system, a FAMP stack, has been installed one may use the following 
+# security script to enhance the overall security:
+#
 # https://github.com/Adminbyaccident/FAMP/blob/master/apache-hardening.sh
 #
-# At a later time you may want to increase Nextcloud's performance by enhancing
-# Apache's HTTP configuration with the Event MPM and enhancing the DB performance
-# which is very useful when file count increases by using Redis or similar cache
-# programs.
+# 
+# Apache's HTTP configuration with the Event MPM is more performant than the pre-fork one.
+# The DB performance can be improved, which is very useful when file count increases 
+# by using Redis or similar cache programs.
 #
 # Remember to adapt this script to your needs wether you are using a domain or 
 # an ip to access your Nextcloud instance.
@@ -45,7 +49,7 @@ pkg install -y php74-zip php74-mbstring php74-gd php74-zlib php74-curl php74-ope
 
 # Install Nextcloud
 # Fetch Nextcloud
-fetch -o /usr/local/www/nextcloud-19.0.3.zip https://download.nextcloud.com/server/releases/nextcloud-19.0.3.zip
+fetch -o /usr/local/www/nextcloud-20.0.1.zip https://download.nextcloud.com/server/releases/nextcloud-20.0.1.zip
 
 # Unzip Nextcloud
 unzip -d /usr/local/www/ /usr/local/www/nextcloud-19.0.3.zip
@@ -68,9 +72,41 @@ AcceptPathInfo On
 # Enable VirtualHost
 sed -i -e '/httpd-vhosts.conf/s/#Include/Include/' /usr/local/etc/apache24/httpd.conf
 
+# Make a backup of the current httpd-vhosts (virtual host) configuration file
+cp /usr/local/etc/apache24/extra/httpd-vhosts.conf /usr/local/etc/apache24/extra/httpd-vhosts.conf.bckp
+
+# Remove the original virtual host file (we've made a backup to restore from, don't panic)
+rm /usr/local/etc/apache24/extra/httpd-vhosts.conf
+
+# Create a new empty virtual host file:
+
 # Set a VirtualHost configuration for Nextcloud
 
 echo "
+# Virtual Hosts
+#
+# Required modules: mod_log_config
+
+# If you want to maintain multiple domains/hostnames on your
+# machine you can setup VirtualHost containers for them. Most configurations
+# use only name-based virtual hosts so the server doesn't need to worry about
+# IP addresses. This is indicated by the asterisks in the directives below.
+#
+
+# Please see the documentation at
+# <URL:http://httpd.apache.org/docs/2.4/vhosts/>
+# for further details before you try to setup virtual hosts.
+#
+# You may use the command line option '-S' to verify your virtual host
+# configuration.
+
+#
+# VirtualHost example:
+# Almost any Apache directive may go into a VirtualHost container.
+# The first VirtualHost section is used for all requests that do not
+# match a ServerName or ServerAlias in any <VirtualHost> block.
+#
+
 <VirtualHost *:80>
     ServerName Nextcloud
     ServerAlias Nextcloud
@@ -82,6 +118,7 @@ echo "
     RewriteRule ^/?(.*) https://%{SERVER_NAME}/$1 [R,L]
     Protocols h2 h2c http/1.1
 </VirtualHost>
+
 <VirtualHost *:443>
     ServerName Nextcloud
     ServerAlias Nextcloud
@@ -90,7 +127,8 @@ echo "
     SSLProtocol all -SSLv2 -SSLv3 -TLSv1 -TLSv1.1
     SSLHonorCipherOrder on
     SSLCipherSuite  ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384
-    SSLCertificateFile "/usr/local/etc/apache24/server.crt"
+    Include /usr/local/etc/apache24/Includes/headers.conf
+	SSLCertificateFile "/usr/local/etc/apache24/server.crt"
     SSLCertificateKeyFile "/usr/local/etc/apache24/server.key"
     ErrorLog "/var/log/nextcloud-error_log"
     CustomLog "/var/log/nextcloud-access_log" common
