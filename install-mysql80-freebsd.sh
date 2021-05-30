69d637d0e7b8435736fcad20c64b0b0158d1db3a
Albert Valbuena <46286613+Adminbyaccident@users.noreply.github.com>
2021-05-30 19:54:28 +0200

    Update install-mysql80-freebsd.sh
    Added comment

--- install-mysql80-freebsd.sh
+++ install-mysql80-freebsd.sh
@@ -55,6 +55,7 @@ expect eof
 
 echo "$SECURE_MYSQL"
 
+# Display the location of the generated root password for MySQL
 echo "Your DB_ROOT_PASSWORD is written on this file /root/db_root_pwd.txt"
 
 # No one but root can read this file. Read only permissions.
