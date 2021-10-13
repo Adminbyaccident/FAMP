3c32b7fe22f31435a9c161d664ec9a245384077f
Albert Valbuena <46286613+Adminbyaccident@users.noreply.github.com>
2021-10-13 22:54:08 +0200

    Update enable-tls-self-signed.sh
    Added HTTP to HTTPS redirection

--- enable-tls-self-signed.sh
+++ enable-tls-self-signed.sh
@@ -17,11 +17,27 @@ sed -i -e '/httpd-ssl.conf/s/#Include/Include/' /usr/local/etc/apache24/httpd.co
 # Enable the SSL/TLS cache module
 sed -i -e '/mod_socache_shmcb.so/s/#LoadModule/LoadModule/' /usr/local/etc/apache24/httpd.conf
 
+# 5.- Enabling the rewrite module
+sed -i -e '/mod_rewrite.so/s/#LoadModule/LoadModule/' /usr/local/etc/apache24/httpd.conf
+
 # Create a self-signed certificate and key for Apache HTTP (ADAPT Organization, Common Name and Email)
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /usr/local/etc/apache24/server.key -out /usr/local/etc/apache24/server.crt -subj "/C=ES/ST=Barcelona/L=Terrassa/O=Adminbyaccident.com/CN=example.com/emailAddress=youremail@gmail.com"
 
 # Restart Apache HTTP to make changes effective
 service apache24 restart
 
+# Redirect from port 80 to port 443 to get HTTPS
+echo "
+# 301 Redirection from port 80 to port 443
+RewriteEngine On
+RewriteCond %{HTTPS} !=on
+RewriteRule ^(.*)$ https://%{HTTP_HOST}$1 [R=301,L]
+" >> /usr/local/etc/apache24/httpd.conf
+
+# Restart Apache HTTP to apply changes
+service apache24 restart
+
 # Inform the user SSL/TLS is now enabled on this server.
 echo "SSL/TLS has been enabled on this server"
+
+# EOF
