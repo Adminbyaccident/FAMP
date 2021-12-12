aeadcc2a2e7b215962f0f1c6753d746ae4d2485d
Albert Valbuena <46286613+Adminbyaccident@users.noreply.github.com>
2021-12-12 18:56:33 +0100

    Added file header

--- event--mariadb-php-fpm-unix-socket.sh
+++ event--mariadb-php-fpm-unix-socket.sh
@@ -1,10 +1,31 @@
-#!/bin/sh
-
-# Instructions on how to use this script 
-
+#!/usr/bin/bash
+# Instructions on how to use this script:
 # chmod +x SCRIPTNAME.sh
-
 # sudo ./SCRIPTNAME.sh
+#
+# SCRIPT: event-mariadb-php-fpm-unix-socket.sh
+# AUTHOR: ALBERT VALBUENA
+# DATE: 17-11-2021
+# SET FOR: Production
+# (For Alpha, Beta, Dev, Test and Production)
+#
+# PLATFORM: FreeBSD 12/13
+#
+# PURPOSE: This script installs a full FAMP stack with Apache HTTP configured with MPM as Event + MariaDB + PHP-FPM configured to read from the UNIX socket
+#
+# REV LIST:
+# DATE: 12-12-2021
+# BY: ALBERT VALBUENA
+# MODIFICATION: 12-12-2021
+#
+#
+# set -n # Uncomment to check your syntax, without execution.
+# # NOTE: Do not forget to put the comment back in or
+# # the shell script will not execute!
+
+##########################################################
+################ BEGINNING OF MAIN #######################
+##########################################################
 
 # Change the default pkg repository from quarterly to latest
 sed -ip 's/quarterly/latest/g' /etc/pkg/FreeBSD.conf
