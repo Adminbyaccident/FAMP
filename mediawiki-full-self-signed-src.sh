870a9c779f516deafa0db14dc6506ab8c5b0a5e1
Albert Valbuena <46286613+Adminbyaccident@users.noreply.github.com>
2021-12-12 19:09:35 +0100

    Added file header

--- mediawiki-full-self-signed-src.sh
+++ mediawiki-full-self-signed-src.sh
@@ -1,10 +1,31 @@
 #!/bin/sh
-
-# Instructions on how to use this script 
-
+# Instructions on how to use this script:
 # chmod +x SCRIPTNAME.sh
-
 # sudo ./SCRIPTNAME.sh
+#
+# SCRIPT: mediawiki-full-self-signed-pkg.sh
+# AUTHOR: ALBERT VALBUENA
+# DATE: 30-05-2021
+# SET FOR: Production
+# (For Alpha, Beta, Dev, Test and Production)
+#
+# PLATFORM: FreeBSD 12/13
+#
+# PURPOSE: This script installs MediaWiki from the originon top of a FAMP stack reading on UNIX socket
+#
+# REV LIST:
+# DATE: 12-12-2021
+# BY: ALBERT VALBUENA
+# MODIFICATION: 12-12-2021
+#
+#
+# set -n # Uncomment to check your syntax, without execution.
+# # NOTE: Do not forget to put the comment back in or
+# # the shell script will not execute!
+
+##########################################################
+################ BEGINNING OF MAIN #######################
+##########################################################
 
 # Change the default pkg repository from quarterly to latest
 sed -ip 's/quarterly/latest/g' /etc/pkg/FreeBSD.conf
