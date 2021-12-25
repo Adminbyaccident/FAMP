fcdb16a4c442069f3407281f8a93836f2285f70e
Albert Valbuena <46286613+Adminbyaccident@users.noreply.github.com>
2021-12-25 19:01:30 +0100

    Update install-mariadb10.5-freebsd.sh

--- install-mariadb10.5-freebsd.sh
+++ install-mariadb10.5-freebsd.sh
@@ -47,6 +47,14 @@ service mysql-server start
 # Install the 'old fashioned' Expect to automate the mysql_secure_installation part
 pkg install -y expect
 
+# Uncomment the line below if you prefer to have a password protected access to MariaDB
+# instead of just using privileges inherited from using the system's root account.
+# Do also uncomment the second expect script and comment out the first one when enabling this below.
+# More details here: https://mariadb.com/kb/en/authentication-plugin-unix-socket/
+
+#pkg install -y pwgen
+#DB_ROOT_PASSWORD=$(pwgen 32 --secure --numerals --capitalize) && export DB_ROOT_PASSWORD && echo $DB_ROOT_PASSWORD >> /root/db_root_pwd.txt
+
 # Make the hideous 'safe' install for MySQL
 echo "Performing MariaDB secure install"
 
@@ -69,5 +77,32 @@ expect eof
 ")
 
 echo "$SECURE_MARIADB"
+
+#SECURE_MARIADB=$(expect -c "
+#set timeout 2
+#spawn mysql_secure_installation
+#expect \"Switch to unix_socket authentication\"
+#send \"y\r\"
+#expect \"Change the root password?\"
+#send \"y\r\"
+#expect \"New password\r\"
+#send \"$DB_ROOT_PASSWORD\r\"
+#expect \"Re-enter new password:\r\"
+#send \"$DB_ROOT_PASSWORD\r\"
+#expect \"Remove anonymous users?\"
+#send \"y\r\"
+#expect \"Disallow root login remotely?\"
+#send \"y\r\"
+#expect \"Remove test database and access to it?\"
+#send \"y\r\"
+#expect \"Reload privilege tables now?\"
+#send \"y\r\"
+#expect eof
+#")
+
+#echo "$SECURE_MARIADB"
+
+# No one but root can read this file. Read only permission. Uncomment if pwgen is used for the DB password generation.
+# chmod 400 /root/db_root_pwd.txt
 
 # EOF
