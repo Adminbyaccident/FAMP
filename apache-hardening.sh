3a04dc4a0ee91aa2873a0e924dd6047be2a1a121
Albert Valbuena <46286613+Adminbyaccident@users.noreply.github.com>
2022-08-03 17:55:54 +0200

    Update apache-hardening.sh
    Removed the use of GNUsed in favor of bases system sed.

--- apache-hardening.sh
+++ apache-hardening.sh
@@ -6,7 +6,7 @@
 # SCRIPT: apache-hardening.sh
 # AUTHOR: ALBERT VALBUENA
 # DATE: 20-02-2021
-# SET FOR: Production
+# SET FOR: Dev
 # (For Alpha, Beta, Dev, Test and Production)
 #
 # PLATFORM: FreeBSD 12/13
@@ -14,9 +14,9 @@
 # PURPOSE: This is a hardening script for Apache HTTP
 #
 # REV LIST:
-# DATE: 12-12-2021
+# DATE: 03-08-2022
 # BY: ALBERT VALBUENA
-# MODIFICATION: 12-12-2021
+# MODIFICATION: 03-08-2022
 #
 #
 # set -n # Uncomment to check your syntax, without execution.
@@ -38,21 +38,19 @@
 # 7.- It installs the Mod_Evasive module in Apache HTTP to help mitigate DoS attacks
 # 8.- It installs the Mod_Security module in Apache HTTP as a Web Application Firewall (WAF) setup with the default rules. Adjust to your needs.
 
-# Install GNU sed to circumvent some of the syntax challenges the BSD sed has
-# such as inserting a line of text in a specific location needing a new line, etc.
-pkg install -y gsed
 
-# Be aware we are using GNU sed here. 
 # When inserting lines do it from bottom to top or inserting new lines can disrupt
 # the default order of a file, eventually breaking the configuration.
-# Consider using echo instead.
+# Consider using echo instead, which will place the line at the bottom of the file.
 
 # 1.- Removing the OS type and modifying version banner (no mod_security here). 
 # 1.1- ServerTokens will only display the minimal information possible.
-gsed -i '227i\ServerTokens Prod' /usr/local/etc/apache24/httpd.conf
+sed -i '' -e '227i\
+ServerTokens Prod' /usr/local/etc/apache24/httpd.conf
 
 # 1.2- ServerSignature will disable the server exposing its type.
-gsed -i '228i\ServerSignature Off' /usr/local/etc/apache24/httpd.conf
+sed -i '' -e '228i\
+ServerSignature Off' /usr/local/etc/apache24/httpd.conf
 
 # Alternatively we can inject the line at the bottom of the file using the echo command.
 # This is a safer option if you make heavy changes at the top of the file.
@@ -67,31 +65,8 @@ sed -i '' -e 's/Options Indexes FollowSymLinks/Options -Indexes +FollowSymLinks 
 
 # 4.- Enabling TLS connections with a self signed certificate. 
 # 4.1- Key and certificate generation
-# Because this is a process where manual interaction is required let's make use of Expect so no hands are needed.
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /usr/local/etc/apache24/server.key -out /usr/local/etc/apache24/server.crt -subj "/C=ES/ST=Barcelona/L=Terrassa/O=Adminbyaccident.com/CN=example.com/emailAddress=youremail@gmail.com"
 
-pkg install -y expect
-
-SECURE_APACHE=$(expect -c "
-set timeout 10
-spawn openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /usr/local/etc/apache24/server.key -out /usr/local/etc/apache24/server.crt
-expect \"Country Name (2 letter code) \[AU\]:\"
-send \"ES\r\"
-expect \"State or Province Name (full name) \[Some-State\]:\"
-send \"Barcelona\r\"
-expect \"Locality Name (eg, city) \[\]:\"
-send \"Terrassa\r\"
-expect \"Organization Name (eg, company) \[Internet Widgits Pty Ltd\]:\"
-send \"Adminbyaccident.com\r\"
-expect \"Organizational Unit Name (eg, section) \[\]:\"
-send \"Operations\r\"
-expect \"Common Name (e.g. server FQDN or YOUR name) \[\]:\"
-send \"Albert Valbuena\r\"
-expect \"Email Address \[\]:\"
-send \"thewhitereflex@gmail.com\r\"
-expect eof
-")
-
-echo "$SECURE_APACHE"
 
 # Because we have generated a certificate + key we will enable SSL/TLS in the server.
 # 4.3- Enabling TLS connections in the server.
@@ -109,9 +84,12 @@ sed -i -e '/mod_rewrite.so/s/#LoadModule/LoadModule/' /usr/local/etc/apache24/ht
 
 # 4.6.2- Adding the redirection rules.
 # Use the following GNU sed entries if you are using the event-php-fpm.sh script.
-gsed -i '181i\RewriteEngine On' /usr/local/etc/apache24/httpd.conf
-gsed -i '182i\RewriteCond %{HTTPS}  !=on' /usr/local/etc/apache24/httpd.conf
-gsed -i '183i\RewriteRule ^/?(.*) https://%{SERVER_NAME}/$1 [R,L]' /usr/local/etc/apache24/httpd.conf
+sed -i '' -e '181i\
+RewriteEngine On' /usr/local/etc/apache24/httpd.conf
+sed -i '' -e '182i\
+RewriteCond %{HTTPS}  !=on' /usr/local/etc/apache24/httpd.conf
+sed -i '' -e '183i\
+RewriteRule ^/?(.*) https://%{SERVER_NAME}/$1 [R,L]' /usr/local/etc/apache24/httpd.conf
 
 # Use the following entries (by uncommenting these and commenting the above GNU sed ones) 
 # if you are using the stdard-famp.sh script, or things will break.
@@ -145,9 +123,12 @@ Include /usr/local/etc/apache24/Includes/headers.conf
 echo 'TraceEnable off' >> /usr/local/etc/apache24/httpd.conf
 
 # 7.- Allow specific HTTP methods.
-gsed -i '269i\    <LimitExcept GET POST HEAD>' /usr/local/etc/apache24/httpd.conf
-gsed -i '270i\       deny from all' /usr/local/etc/apache24/httpd.conf
-gsed -i '271i\    </LimitExcept>' /usr/local/etc/apache24/httpd.conf
+sed -i '' -e '269i\
+<LimitExcept GET POST HEAD>' /usr/local/etc/apache24/httpd.conf
+sed -i '' -e '270i\
+deny from all' /usr/local/etc/apache24/httpd.conf
+sed -i '' -e '271i\
+</LimitExcept>' /usr/local/etc/apache24/httpd.conf
 
 # 8.- Restart Apache HTTP so changes take effect.
 service apache24 restart
@@ -182,17 +163,13 @@ touch /usr/local/etc/apache24/extra/httpd-security.conf
 
 echo "
 <IfModule mod_rewrite.c>
-
 RewriteEngine on
-
 # Condition to block suspicious request methods.
 RewriteCond %{REQUEST_METHOD} ^(HEAD|TRACE|DELETE|TRACK|DEBUG) [NC,OR]
-
 # Condition to block the specified user agents from programs and bots.
 RewriteCond %{HTTP_USER_AGENT} (havij|libwww-perl|wget|python|nikto|curl|scan|java|winhttp|clshttp|loader|fetch) [NC,OR]
 RewriteCond %{HTTP_USER_AGENT} (%0A|%0D|%27|%3C|%3E|%00) [NC,OR]
 RewriteCond %{HTTP_USER_AGENT} (;|<|>|'|\"|\)|\(|%0A|%0D|%22|%27|%28|%3C|%3E|%00).*(libwww-perl|wget|python|nikto|curl|scan|java|winhttp|HTTrack|clshttp|archiver|loader|email|harvest|extract|grab|miner) [NC,OR]
-
 # Condition to block suspicious header requests.
 RewriteCond %{HTTP_ACCEPT} (localhost|loopback|127\.0\.0\.1) [NC,OR]
 RewriteCond %{HTTP_COOKIE} (localhost|loopback|127\.0\.0\.1) [NC,OR]
@@ -200,7 +177,6 @@ RewriteCond %{HTTP_FORWARDED} (localhost|loopback|127\.0\.0\.1) [NC,OR]
 RewriteCond %{HTTP_HOST} (localhost|loopback|127\.0\.0\.1) [NC,OR]
 RewriteCond %{HTTP_PROXY_CONNECTION} (localhost|loopback|127\.0\.0\.1) [NC,OR]
 RewriteCond %{HTTP_REFERER} (localhost|loopback|127\.0\.0\.1) [NC,OR]
-
 # Condition to block Proxy/LoadBalancer/WAF bypass
 RewriteCond %{HTTP:X-Client-IP} (localhost|loopback|127\.0\.0\.1) [NC,OR]
 RewriteCond %{HTTP:X-Forwarded-For} (localhost|loopback|127\.0\.0\.1) [NC,OR]
@@ -211,39 +187,29 @@ RewriteCond %{HTTP:X-Originating-IP} (localhost|loopback|127\.0\.0\.1) [NC,OR]
 RewriteCond %{HTTP:X-Forwarded-From} (localhost|loopback|127\.0\.0\.1) [NC,OR]
 RewriteCond %{HTTP:X-Forwarded-Host} (localhost|loopback|127\.0\.0\.1) [NC,OR]
 RewriteCond %{HTTP:X-Remote-Addr} (localhost|loopback|127\.0\.0\.1) [NC,OR]
-
 # Condition to block requests that incorporate the specified expressions in them. Avoid injection.
 RewriteCond %{THE_REQUEST} (\?|\*|%2a)+(%20+|\\s+|%20+\\s+|\\s+%20+|\\s+%20+\\s+)(http|https)(:/|/) [NC,OR]
-
 # Condition to block any request containing the etc/passwd string and avoid system passwords exfiltration.
 RewriteCond %{THE_REQUEST} etc/passwd [NC,OR]
-
 # Condition to block the execution of CGI programs.
 RewriteCond %{THE_REQUEST} cgi-bin [NC,OR]
-
 # Condition to block requests that jump into the next line. Avoid injection.
 RewriteCond %{THE_REQUEST} (%0A|%0D|\\r|\\n) [NC,OR]
-
 # Condition to block any Sharepoint services call.
 RewriteCond %{REQUEST_URI} owssvr\.dll [NC,OR]
-
 # Condition to block requests that simulate to come from the specified expressions. Avoid injection.
 RewriteCond %{HTTP_REFERER} (%0A|%0D|%27|%3C|%3E|%00) [NC,OR]
 RewriteCond %{HTTP_REFERER} \.opendirviewer\. [NC,OR]
 RewriteCond %{HTTP_REFERER} users\.skynet\.be.* [NC,OR]
-
 # Condition to block requests that incorporate the specified expressions in them.
 RewriteCond %{QUERY_STRING} [a-zA-Z0-9_]=(http|https):// [NC,OR]
 RewriteCond %{QUERY_STRING} [a-zA-Z0-9_]=(\.\.//?)+ [NC,OR]
 RewriteCond %{QUERY_STRING} [a-zA-Z0-9_]=/([a-z0-9_.]//?)+ [NC,OR]
-
 # Condition to block any PHP execution. Avoid injection.
 RewriteCond %{QUERY_STRING} \=PHP[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12} [NC,OR]
 RewriteCond %{QUERY_STRING} (\.\./|%2e%2e%2f|%2e%2e/|\.\.%2f|%2e\.%2f|%2e\./|\.%2e%2f|\.%2e/) [NC,OR]
-
 # Condition to block FTP usage. Avoid uploads.
 RewriteCond %{QUERY_STRING} ftp\: [NC,OR]
-
 # Condition to block any requests jumping over paths or injecting/retrieving objects.
 RewriteCond %{QUERY_STRING} (http|https)\: [NC,OR]
 RewriteCond %{QUERY_STRING} \=\|w\| [NC,OR]
@@ -257,31 +223,23 @@ RewriteCond %{QUERY_STRING} (\<|%3C).*object.*(\>|%3E) [NC,OR]
 RewriteCond %{QUERY_STRING} (<|%3C)([^o]*o)+bject.*(>|%3E) [NC,OR]
 RewriteCond %{QUERY_STRING} (\<|%3C).*iframe.*(\>|%3E) [NC,OR]
 RewriteCond %{QUERY_STRING} (<|%3C)([^i]*i)+frame.*(>|%3E) [NC,OR]
-
 # Condition to block with the intention to en/de-code strings in base64
 RewriteCond %{QUERY_STRING} base64_encode.*\(.*\) [NC,OR]
 RewriteCond %{QUERY_STRING} base64_(en|de)code[^(]*\([^)]*\) [NC,OR]
-
 # Condition to block
 RewriteCond %{QUERY_STRING} GLOBALS(=|\[|\%[0-9A-Z]{0,2}) [OR]
 RewriteCond %{QUERY_STRING} _REQUEST(=|\[|\%[0-9A-Z]{0,2}) [OR]
-
 # Condition to block requests that incorporate the specified expressions in them. Avoid injection.
 RewriteCond %{QUERY_STRING} ^.*(\(|\)|<|>|%3c|%3e).* [NC,OR]
 RewriteCond %{QUERY_STRING} ^.*(\x00|\x04|\x08|\x0d|\x1b|\x20|\x3c|\x3e|\x7f).* [NC,OR]
-
 # Condition to block requests which declare the specified values in the string query. Avoid injection.
 RewriteCond %{QUERY_STRING} (NULL|OUTFILE|LOAD_FILE) [OR]
-
 # Condition to block requests intending to retrieve or inject content in the motd file or /etc and /bin directories.
 RewriteCond %{QUERY_STRING} (\.{1,}/)+(motd|etc|bin) [NC,OR]
-
 # Condition to block any string referencing to the host or loopback interface.
 RewriteCond %{QUERY_STRING} (localhost|loopback|127\.0\.0\.1) [NC,OR]
-
 # Condition to block requests that incorporate the specified expressions in them. Avoid injection.
 RewriteCond %{QUERY_STRING} (<|>|'|%0A|%0D|%27|%3C|%3E|%00) [NC,OR]
-
 # Condition to block SQL injection attacks 
 RewriteCond %{QUERY_STRING} concat[^\(]*\( [NC,OR]
 RewriteCond %{QUERY_STRING} union([^s]*s)+elect [NC,OR]
@@ -289,10 +247,8 @@ RewriteCond %{QUERY_STRING} union([^a]*a)+ll([^s]*s)+elect [NC,OR]
 RewriteCond %{QUERY_STRING} \-[sdcr].*(allow_url_include|allow_url_fopen|safe_mode|disable_functions|auto_prepend_file) [NC,OR]
 RewriteCond %{QUERY_STRING} (;|<|>|'|\"|\)|%0A|%0D|%22|%27|%3C|%3E|%00).*(/\*|union|select|insert|drop|delete|update|cast|create|char|convert|alter|declare|order|script|set|md5|benchmark|encode) [NC,OR]
 RewriteCond %{QUERY_STRING} (sp_executesql) [NC]
-
 # The rewrite rule itself. Any match gets blocked.
 RewriteRule ^(.*)$ - [F]
-
 </IfModule>
 " >> /usr/local/etc/apache24/extra/nextcloud-security.conf
 
