638a902304e59ff2649abecb9e6260d1d95c8267
Albert Valbuena <46286613+Adminbyaccident@users.noreply.github.com>
2023-01-31 20:42:59 +0100

    Update apache-hardening.sh
    Updated the whole thing

--- apache-hardening.sh
+++ apache-hardening.sh
@@ -14,9 +14,9 @@
 # PURPOSE: This is a hardening script for Apache HTTP
 #
 # REV LIST:
-# DATE: 03-08-2022
+# DATE: 31-01-2023
 # BY: ALBERT VALBUENA
-# MODIFICATION: 03-08-2022
+# MODIFICATION: 31-01-2023
 #
 #
 # set -n # Uncomment to check your syntax, without execution.
@@ -27,35 +27,34 @@
 ###########################################  WARNING !!!  #################################################
 ###########################################################################################################
 
+echo '
+
+###########################################################################################################
+###########################################  WARNING !!!  #################################################
+###########################################################################################################
+
+THIS SCRIPT WILL EXECUTE FOLLOWING THESE NEXT STEPS IN 30 SECONDS.
+
+YOU HAVE BEEN WARNED!
+
 # Use this script at your own discretion. It modifies important bits of your Apache HTTP configuration
-# For example it changes:
-# 1.- Information provided by your server
-# 2.- It installs a self-signed certificate (modify this at your own will)
-# 3.- If, of course, enables SSL/TLS connections
-# 4.- Configures HTTP headers to be secure in a strict fashion. This may break some of your desired features. Adjust accordingly.
-# 5.- Disables the TRACE method
-# 6.- Explicitely and exclusively allows the GET, POST and HEAD methods
-# 7.- It installs the Mod_Evasive module in Apache HTTP to help mitigate DoS attacks
-# 8.- It installs the Mod_Security module in Apache HTTP as a Web Application Firewall (WAF) setup with the default rules. Adjust to your needs.
+# For example it will:
+# 1.- Remove information provided by your server such as its version
+# 2.- Hide your PHP version
+# 3.- A self-signed certificate will be installed
+# 4.- SSL/TLS connections will be enabled
+# 5.- HTTP headers will be installed and configured
+# 6.- The TRACE method will be disabled
+# 7.- Explicitely and exclusively allow the GET, POST and HEAD methods
+# 8.- Install the Mod_Evasive module in Apache HTTP to help mitigate DoS attacks
+# 9.- Install the Mod_Security module in Apache HTTP as a Web Application Firewall (WAF) setup with the default rules. Adjust to your needs.
+'
 
+sleep 30
 
-# When inserting lines do it from bottom to top or inserting new lines can disrupt
-# the default order of a file, eventually breaking the configuration.
-# Consider using echo instead, which will place the line at the bottom of the file.
-
-# 1.- Removing the OS type and modifying version banner (no mod_security here). 
-# 1.1- ServerTokens will only display the minimal information possible.
-sed -i '' -e '227i\
-ServerTokens Prod' /usr/local/etc/apache24/httpd.conf
-
-# 1.2- ServerSignature will disable the server exposing its type.
-sed -i '' -e '228i\
-ServerSignature Off' /usr/local/etc/apache24/httpd.conf
-
-# Alternatively we can inject the line at the bottom of the file using the echo command.
-# This is a safer option if you make heavy changes at the top of the file.
-# echo 'ServerTokens Prod' >> /usr/local/etc/apache24/httpd.conf
-# echo 'ServerSignature Off' >> /usr/local/etc/apache24/httpd.conf
+# 1.- Removing the OS type and modifying version banner 
+echo 'ServerTokens Prod' >> /usr/local/etc/apache24/httpd.conf
+echo 'ServerSignature Off' >> /usr/local/etc/apache24/httpd.conf
 
 # 2.- Avoid PHP's information (version, etc) being disclosed
 sed -i -e '/expose_php/s/expose_php = On/expose_php = Off/' /usr/local/etc/php.ini
@@ -65,8 +64,7 @@ sed -i '' -e 's/Options Indexes FollowSymLinks/Options -Indexes +FollowSymLinks 
 
 # 4.- Enabling TLS connections with a self signed certificate. 
 # 4.1- Key and certificate generation
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /usr/local/etc/apache24/server.key -out /usr/local/etc/apache24/server.crt -subj "/C=ES/ST=Barcelona/L=Terrassa/O=Adminbyaccident.com/CN=example.com/emailAddress=youremail@gmail.com"
-
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /usr/local/etc/apache24/server.key -out /usr/local/etc/apache24/server.crt -subj "/C=ES/ST=Barcelona/L=Terrassa/O=Adminbyaccident.com/CN=example.com/emailAddress=yourmail@gmail.com"
 
 # Because we have generated a certificate + key we will enable SSL/TLS in the server.
 # 4.3- Enabling TLS connections in the server.
@@ -82,20 +80,10 @@ sed -i -e '/mod_socache_shmcb.so/s/#LoadModule/LoadModule/' /usr/local/etc/apach
 # 4.6.1- Enabling the rewrite module
 sed -i -e '/mod_rewrite.so/s/#LoadModule/LoadModule/' /usr/local/etc/apache24/httpd.conf
 
-# 4.6.2- Adding the redirection rules.
-# Use the following GNU sed entries if you are using the event-php-fpm.sh script.
-sed -i '' -e '181i\
-RewriteEngine On' /usr/local/etc/apache24/httpd.conf
-sed -i '' -e '182i\
-RewriteCond %{HTTPS}  !=on' /usr/local/etc/apache24/httpd.conf
-sed -i '' -e '183i\
-RewriteRule ^/?(.*) https://%{SERVER_NAME}/$1 [R,L]' /usr/local/etc/apache24/httpd.conf
-
-# Use the following entries (by uncommenting these and commenting the above GNU sed ones) 
-# if you are using the stdard-famp.sh script, or things will break.
-# echo 'RewriteEngine On' /usr/local/etc/apache24/httpd.conf
-# echo 'RewriteCond %{HTTPS}  !=on' /usr/local/etc/apache24/httpd.conf
-# echo 'RewriteRule ^/?(.*) https://%{SERVER_NAME}/$1 [R,L]' /usr/local/etc/apache24/httpd.conf
+# 4.6.2- Adding the redirection rules from HTTP to HTTPS.
+echo 'RewriteEngine On' >> /usr/local/etc/apache24/httpd.conf
+echo 'RewriteCond %{HTTPS}  !=on' >> /usr/local/etc/apache24/httpd.conf
+echo 'RewriteRule ^/?(.*) https://%{SERVER_NAME}/$1 [R,L]' >> /usr/local/etc/apache24/httpd.conf
 
 # 5.- Secure headers
 echo "
@@ -123,23 +111,23 @@ Include /usr/local/etc/apache24/Includes/headers.conf
 echo 'TraceEnable off' >> /usr/local/etc/apache24/httpd.conf
 
 # 7.- Allow specific HTTP methods.
-sed -i '' -e '269i\
+sed -i '' -e '272i\
 <LimitExcept GET POST HEAD>' /usr/local/etc/apache24/httpd.conf
-sed -i '' -e '270i\
+sed -i '' -e '273i\
 deny from all' /usr/local/etc/apache24/httpd.conf
-sed -i '' -e '271i\
+sed -i '' -e '274i\
 </LimitExcept>' /usr/local/etc/apache24/httpd.conf
 
-# 8.- Restart Apache HTTP so changes take effect.
+# Restart Apache HTTP so changes take effect.
 service apache24 restart
 
-# 9.- Install mod_evasive
+# 8.- Install mod_evasive
 pkg install -y ap24-mod_evasive
 
-# 9.1- Enable the mod_evasive module in Apache HTTP
+# 8.1- Enable the mod_evasive module in Apache HTTP
 sed -i -e '/mod_evasive20.so/s/#LoadModule/LoadModule/' /usr/local/etc/apache24/httpd.conf
 
-# 9.2- Configure the mod_evasive module
+# 8.2- Configure the mod_evasive module
 touch /usr/local/etc/apache24/modules.d/020-mod_evasive.conf
 
 echo "<IfModule mod_evasive20.c>
@@ -150,67 +138,40 @@ echo "<IfModule mod_evasive20.c>
 	DOSSiteInterval 1
 	DOSBlockingPeriod 360
 	DOSEmailNotify youremail@address.com
-	DOSSystemCommand "su – root -c /sbin/ipfw add 50000 deny %s to any in"
-	DOSLogDir "/var/log/mod_evasive"
+	DOSSystemCommand \"su – root -c /sbin/ipfw add 50000 deny %s to any in\"
+	DOSLogDir \"/var/log/mod_evasive\"
 </IfModule>" >> /usr/local/etc/apache24/modules.d/020-mod_evasive.conf
 
-# 9.4- Restart Apache for the configuration to take effect
+# 8.4- Restart Apache for the configuration to take effect
 apachectl graceful
 
-# 10.- WAF-like rules enablement for any generic FAMP stack. Applicable to WPress, Drupal, Joomla, Nextcloud and the likes of those.
+# 9.- WAF-like rules enablement for any generic FAMP stack. Applicable to WPress, Drupal, Joomla, Nextcloud and the likes of those.
 # Create an empty httpd-security.conf file
 touch /usr/local/etc/apache24/extra/httpd-security.conf
 
+# 9.1- Enabling the rewrite module
+sed -i -e '/mod_rewrite.so/s/#LoadModule/LoadModule/' /usr/local/etc/apache24/httpd.conf
+
 echo "
 <IfModule mod_rewrite.c>
-RewriteEngine on
-# Condition to block suspicious request methods.
-RewriteCond %{REQUEST_METHOD} ^(HEAD|TRACE|DELETE|TRACK|DEBUG) [NC,OR]
-# Condition to block the specified user agents from programs and bots.
-RewriteCond %{HTTP_USER_AGENT} (havij|libwww-perl|wget|python|nikto|curl|scan|java|winhttp|clshttp|loader|fetch) [NC,OR]
+RewriteEngine On
+RewriteCond %{HTTP_USER_AGENT} (havij|libwww-perl|wget|python|nikto|curl|scan|java|winhttp|clshttp|loader) [NC,OR]
 RewriteCond %{HTTP_USER_AGENT} (%0A|%0D|%27|%3C|%3E|%00) [NC,OR]
-RewriteCond %{HTTP_USER_AGENT} (;|<|>|'|\"|\)|\(|%0A|%0D|%22|%27|%28|%3C|%3E|%00).*(libwww-perl|wget|python|nikto|curl|scan|java|winhttp|HTTrack|clshttp|archiver|loader|email|harvest|extract|grab|miner) [NC,OR]
-# Condition to block suspicious header requests.
-RewriteCond %{HTTP_ACCEPT} (localhost|loopback|127\.0\.0\.1) [NC,OR]
-RewriteCond %{HTTP_COOKIE} (localhost|loopback|127\.0\.0\.1) [NC,OR]
-RewriteCond %{HTTP_FORWARDED} (localhost|loopback|127\.0\.0\.1) [NC,OR]
-RewriteCond %{HTTP_HOST} (localhost|loopback|127\.0\.0\.1) [NC,OR]
-RewriteCond %{HTTP_PROXY_CONNECTION} (localhost|loopback|127\.0\.0\.1) [NC,OR]
-RewriteCond %{HTTP_REFERER} (localhost|loopback|127\.0\.0\.1) [NC,OR]
-# Condition to block Proxy/LoadBalancer/WAF bypass
-RewriteCond %{HTTP:X-Client-IP} (localhost|loopback|127\.0\.0\.1) [NC,OR]
-RewriteCond %{HTTP:X-Forwarded-For} (localhost|loopback|127\.0\.0\.1) [NC,OR]
-RewriteCond %{HTTP:X-Forwarded-Scheme} (localhost|loopback|127\.0\.0\.1) [NC,OR]
-RewriteCond %{HTTP:X-Real-IP} (localhost|loopback|127\.0\.0\.1) [NC,OR]
-RewriteCond %{HTTP:X-Forwarded-By} (localhost|loopback|127\.0\.0\.1) [NC,OR]
-RewriteCond %{HTTP:X-Originating-IP} (localhost|loopback|127\.0\.0\.1) [NC,OR]
-RewriteCond %{HTTP:X-Forwarded-From} (localhost|loopback|127\.0\.0\.1) [NC,OR]
-RewriteCond %{HTTP:X-Forwarded-Host} (localhost|loopback|127\.0\.0\.1) [NC,OR]
-RewriteCond %{HTTP:X-Remote-Addr} (localhost|loopback|127\.0\.0\.1) [NC,OR]
-# Condition to block requests that incorporate the specified expressions in them. Avoid injection.
-RewriteCond %{THE_REQUEST} (\?|\*|%2a)+(%20+|\\s+|%20+\\s+|\\s+%20+|\\s+%20+\\s+)(http|https)(:/|/) [NC,OR]
-# Condition to block any request containing the etc/passwd string and avoid system passwords exfiltration.
+RewriteCond %{HTTP_USER_AGENT} (;|<|>|'|\"|\)|\(|%0A|%0D|%22|%27|%28|%3C|%3E|%00).*(libwww-perl|wget|python|nikto|curl|scan|java|winhttp|HTTrack|clshttp|archiver|loader|email|harvest|extract|grab|miner|fetch) [NC,OR]
+RewriteCond %{THE_REQUEST} (\?|\*|%2a)+(%20+|\\\s+|%20+\\\s+|\\\s+%20+|\\\s+%20+\\\s+)(http|https)(:/|/) [NC,OR]
 RewriteCond %{THE_REQUEST} etc/passwd [NC,OR]
-# Condition to block the execution of CGI programs.
 RewriteCond %{THE_REQUEST} cgi-bin [NC,OR]
-# Condition to block requests that jump into the next line. Avoid injection.
 RewriteCond %{THE_REQUEST} (%0A|%0D|\\r|\\n) [NC,OR]
-# Condition to block any Sharepoint services call.
 RewriteCond %{REQUEST_URI} owssvr\.dll [NC,OR]
-# Condition to block requests that simulate to come from the specified expressions. Avoid injection.
 RewriteCond %{HTTP_REFERER} (%0A|%0D|%27|%3C|%3E|%00) [NC,OR]
 RewriteCond %{HTTP_REFERER} \.opendirviewer\. [NC,OR]
 RewriteCond %{HTTP_REFERER} users\.skynet\.be.* [NC,OR]
-# Condition to block requests that incorporate the specified expressions in them.
 RewriteCond %{QUERY_STRING} [a-zA-Z0-9_]=(http|https):// [NC,OR]
 RewriteCond %{QUERY_STRING} [a-zA-Z0-9_]=(\.\.//?)+ [NC,OR]
 RewriteCond %{QUERY_STRING} [a-zA-Z0-9_]=/([a-z0-9_.]//?)+ [NC,OR]
-# Condition to block any PHP execution. Avoid injection.
 RewriteCond %{QUERY_STRING} \=PHP[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12} [NC,OR]
 RewriteCond %{QUERY_STRING} (\.\./|%2e%2e%2f|%2e%2e/|\.\.%2f|%2e\.%2f|%2e\./|\.%2e%2f|\.%2e/) [NC,OR]
-# Condition to block FTP usage. Avoid uploads.
 RewriteCond %{QUERY_STRING} ftp\: [NC,OR]
-# Condition to block any requests jumping over paths or injecting/retrieving objects.
 RewriteCond %{QUERY_STRING} (http|https)\: [NC,OR]
 RewriteCond %{QUERY_STRING} \=\|w\| [NC,OR]
 RewriteCond %{QUERY_STRING} ^(.*)/self/(.*)$ [NC,OR]
@@ -223,34 +184,35 @@ RewriteCond %{QUERY_STRING} (\<|%3C).*object.*(\>|%3E) [NC,OR]
 RewriteCond %{QUERY_STRING} (<|%3C)([^o]*o)+bject.*(>|%3E) [NC,OR]
 RewriteCond %{QUERY_STRING} (\<|%3C).*iframe.*(\>|%3E) [NC,OR]
 RewriteCond %{QUERY_STRING} (<|%3C)([^i]*i)+frame.*(>|%3E) [NC,OR]
-# Condition to block with the intention to en/de-code strings in base64
 RewriteCond %{QUERY_STRING} base64_encode.*\(.*\) [NC,OR]
 RewriteCond %{QUERY_STRING} base64_(en|de)code[^(]*\([^)]*\) [NC,OR]
-# Condition to block
 RewriteCond %{QUERY_STRING} GLOBALS(=|\[|\%[0-9A-Z]{0,2}) [OR]
 RewriteCond %{QUERY_STRING} _REQUEST(=|\[|\%[0-9A-Z]{0,2}) [OR]
-# Condition to block requests that incorporate the specified expressions in them. Avoid injection.
 RewriteCond %{QUERY_STRING} ^.*(\(|\)|<|>|%3c|%3e).* [NC,OR]
 RewriteCond %{QUERY_STRING} ^.*(\x00|\x04|\x08|\x0d|\x1b|\x20|\x3c|\x3e|\x7f).* [NC,OR]
-# Condition to block requests which declare the specified values in the string query. Avoid injection.
 RewriteCond %{QUERY_STRING} (NULL|OUTFILE|LOAD_FILE) [OR]
-# Condition to block requests intending to retrieve or inject content in the motd file or /etc and /bin directories.
 RewriteCond %{QUERY_STRING} (\.{1,}/)+(motd|etc|bin) [NC,OR]
-# Condition to block any string referencing to the host or loopback interface.
 RewriteCond %{QUERY_STRING} (localhost|loopback|127\.0\.0\.1) [NC,OR]
-# Condition to block requests that incorporate the specified expressions in them. Avoid injection.
-RewriteCond %{QUERY_STRING} (<|>|'|%0A|%0D|%27|%3C|%3E|%00) [NC,OR]
-# Condition to block SQL injection attacks 
+RewriteCond %{QUERY_STRING} (<|>|''|%0A|%0D|%27|%3C|%3E|%00) [NC,OR]
 RewriteCond %{QUERY_STRING} concat[^\(]*\( [NC,OR]
 RewriteCond %{QUERY_STRING} union([^s]*s)+elect [NC,OR]
 RewriteCond %{QUERY_STRING} union([^a]*a)+ll([^s]*s)+elect [NC,OR]
 RewriteCond %{QUERY_STRING} \-[sdcr].*(allow_url_include|allow_url_fopen|safe_mode|disable_functions|auto_prepend_file) [NC,OR]
 RewriteCond %{QUERY_STRING} (;|<|>|'|\"|\)|%0A|%0D|%22|%27|%3C|%3E|%00).*(/\*|union|select|insert|drop|delete|update|cast|create|char|convert|alter|declare|order|script|set|md5|benchmark|encode) [NC,OR]
+# Condition to block Proxy/LoadBalancer/WAF bypass
+RewriteCond %{HTTP:X-Client-IP} (localhost|loopback|127\.0\.0\.1) [NC,OR]
+RewriteCond %{HTTP:X-Forwarded-For} (localhost|loopback|127\.0\.0\.1) [NC,OR]
+RewriteCond %{HTTP:X-Forwarded-Scheme} (localhost|loopback|127\.0\.0\.1) [NC,OR]
+RewriteCond %{HTTP:X-Real-IP} (localhost|loopback|127\.0\.0\.1) [NC,OR]
+RewriteCond %{HTTP:X-Forwarded-By} (localhost|loopback|127\.0\.0\.1) [NC,OR]
+RewriteCond %{HTTP:X-Originating-IP} (localhost|loopback|127\.0\.0\.1) [NC,OR]
+RewriteCond %{HTTP:X-Forwarded-From} (localhost|loopback|127\.0\.0\.1) [NC,OR]
+RewriteCond %{HTTP:X-Forwarded-Host} (localhost|loopback|127\.0\.0\.1) [NC,OR]
+RewriteCond %{HTTP:X-Remote-Addr} (localhost|loopback|127\.0\.0\.1) [NC,OR]
 RewriteCond %{QUERY_STRING} (sp_executesql) [NC]
-# The rewrite rule itself. Any match gets blocked.
 RewriteRule ^(.*)$ - [F]
 </IfModule>
-" >> /usr/local/etc/apache24/extra/nextcloud-security.conf
+" >> /usr/local/etc/apache24/extra/httpd-security.conf
 
 echo "
 Include /usr/local/etc/apache24/extra/httpd-security.conf
@@ -259,39 +221,29 @@ Include /usr/local/etc/apache24/extra/httpd-security.conf
 # Restart Apache HTTP to load the WAF-like configuration.
 service apache24 restart
 
-# 11.- Install Modsecurity 3 for Apache HTTP. 
-# If you apply step 10 there's no much need of ModSecurity unless you really need it.
-# pkg install -y modsecurity3-apache
+# Install ModSecurity
+pkg install -y ap24-mod_security
 
-# Download Git SpiderLab Rules >> OWASP ModSecurity Core Rule Set
-# pkg install -y git
-# git clone https://github.com/coreruleset/coreruleset /usr/local/etc/modsecurity/coreruleset/
-# cp /usr/local/etc/modsecurity/coreruleset/crs-setup.conf.example /usr/local/etc/modsecurity/coreruleset/crs-setup.conf
-# sed -ip 's/SecRuleEngine DetectionOnly/SecRuleEngine On/g' /usr/local/etc/modsecurity/modsecurity.conf
+# Install CRS ruleset for ModSecurity
+pkg install wget 
+wget -O /usr/local/etc/modsecurity/crs-ruleset-3.3.4.zip https://github.com/coreruleset/coreruleset/archive/refs/tags/v3.3.4.zip
+pkg install -y unzip
+unzip /usr/local/etc/modsecurity/crs-ruleset-3.3.4.zip -d /usr/local/etc/modsecurity/
+cp /usr/local/etc/modsecurity/coreruleset-3.3.4/crs-setup.conf.example /usr/local/etc/modsecurity/coreruleset-3.3.4/crs-setup.conf
+cp /usr/local/etc/modsecurity/coreruleset-3.3.4/rules/REQUEST-900-EXCLUSION-RULES-BEFORE-CRS.conf.example /usr/local/etc/modsecurity/coreruleset-3.3.4/rules/REQUEST-900-EXCLUSION-RULES-BEFORE-CRS.conf
+cp /usr/local/etc/modsecurity/coreruleset-3.3.4/rules/RESPONSE-999-EXCLUSION-RULES-AFTER-CRS.conf.example /usr/local/etc/modsecurity/coreruleset-3.3.4/rules/RESPONSE-999-EXCLUSION-RULES-AFTER-CRS.conf
+echo '
+LoadModule security2_module libexec/apache24/mod_security2.so
+Include /usr/local/etc/modsecurity/*.conf
+Include /usr/local/etc/modsecurity/coreruleset-3.3.4/crs-setup.conf
+Include /usr/local/etc/modsecurity/coreruleset-3.3.4/rules/*.conf
+' >> /usr/local/etc/apache24/modules.d/280_mod_security.conf
+sed -i -e '/mod_unique_id.so/s/#LoadModule/LoadModule/g' /usr/local/etc/apache24/httpd.conf
+sed -i -e 'SecRuleEngine DetectionOnly/s/SecRuleEngine DetectionOnly/SecRuleEngine On/g' /usr/local/etc/modsecurity/modsecurity.conf
 
-# Set the configuration files for ModSecurity 3 to work
-# touch /usr/local/etc/apache24/modsecurity-rules.conf
-
-# echo "
-# Include /usr/local/etc/modsecurity/modsecurity.conf
-# Include /usr/local/etc/modsecurity/coreruleset/crs-setup.conf
-# Include /usr/local/etc/modsecurity/coreruleset/rules/*.conf
-# " >> /usr/local/etc/apache24/modsecurity-rules.conf
-
-# Enable ModSecurity's 3 module
-# echo "
-# modsecurity on
-# modsecurity_rules_file /usr/local/etc/apache24/modsecurity-rules.conf
-# " >> /usr/local/etc/apache24/httpd.conf
-
-# Rename 2 config files
-# mv /usr/local/etc/modsecurity/coreruleset/rules/REQUEST-900-EXCLUSION-RULES-BEFORE-CRS.conf.example /usr/local/etc/modsecurity/coreruleset/rules/REQUEST-900-EXCLUSION-RULES-BEFORE-CRS.conf
-
-# mv /usr/local/etc/modsecurity/coreruleset/rules/RESPONSE-999-EXCLUSION-RULES-AFTER-CRS.conf.example /usr/local/etc/modsecurity/coreruleset/rules/RESPONSE-999-EXCLUSION-RULES-AFTER-CRS.conf
-
-# Restart Apache HTTP
-# apachectl restart
+service apache24 restart
 
 ## References:
 ## https://www.adminbyaccident.com/security/how-to-harden-apache-http/
 ## https://www.digitalocean.com/community/tutorials/recommended-steps-to-harden-apache-http-on-freebsd-12-0
+## /usr/local/etc/apache24/modules.d/280_mod_security.conf
