8d7a1dba00408cbcfc411db148b856be3a726e1e
Albert Valbuena <46286613+Adminbyaccident@users.noreply.github.com>
2023-05-06 18:51:26 +0200

    Update self-full-wpress-mariadb.sh
    Updated PHP packages

--- self-full-wpress-mariadb.sh
+++ self-full-wpress-mariadb.sh
@@ -53,8 +53,42 @@ pkg install -y mariadb106-server mariadb106-client
 sysrc mysql_enable="YES"
 sysrc mysql_args="--bind-address=127.0.0.1"
 
-# Install PHP 8.2 and its 'funny' dependencies
-pkg install -y php82 php82-mysqli php82-extensions
+# Install PHP 8.2 and its 'funny' dependencies for WordPress
+pkg install -y	php82\
+		php82-bcmath\
+		php82-bz2\
+		php82-ctype\
+		php82-curl\
+		php82-dom\
+		php82-exif\
+		php82-extensions\
+		php82-fileinfo\
+		php82-filter\
+		php82-ftp\
+		php82-gd\
+		php82-iconv\
+		php82-intl\
+		php82-mbstring\
+		php82-mysqli\
+		php82-opcache\
+		php82-pdo\
+		php82-pdo_mysql\
+		php82-pdo_sqlite\
+		php82-pecl-mcrypt\
+		php82-phar\
+		php82-posix\
+		php82-session\
+		php82-simplexml\
+		php82-soap\
+		php82-sockets\
+		php82-sqlite3\
+		php82-tokenizer\
+		php82-xml\
+		php82-xmlreader\
+		php82-xmlwriter\
+		php82-zip\
+		php82-zlib\
+		php82-pecl-imagick-3.7.0_1
 
 # Install the 'old fashioned' Expect to automate the mysql_secure_installation part
 pkg install -y expect
